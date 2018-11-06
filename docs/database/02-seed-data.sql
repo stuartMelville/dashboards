6fd40bc1bf78f2f8107c73494f4d12ded9924de5
stuar <stuar@DESKTOP-NDP4UHV>
2018-11-06 11:26:13 +0000

    fixed sql host name parameter everything runs in docker-compose

--- docs/database/02-seed-data.sql
+++ docs/database/02-seed-data.sql
@@ -7,9 +7,9 @@ INSERT INTO dashboards (id,createdAt,title,updatedAt)  VALUES(4,'2018-10-04 8:23
 INSERT INTO dashboards (id,createdAt,title,updatedAt)  VALUES(5,'2018-11-05 8:23:12',"Title 5",'2018-11-05 8:23:12');
 INSERT INTO dashboards (id,createdAt,title,updatedAt)  VALUES(6,'2018-11-06 8:23:12',"Title 6",'2018-11-06 8:23:12');
 INSERT INTO dashboards (id,createdAt,title,updatedAt)  VALUES(7,'2018-11-07 8:23:12',"Title 7",'2018-11-07 8:23:12');
-INSERT INTO dashboards (id,createdAt,title,updatedAt)  VALUES(8,'2018-11-08 8:23:12',"Title 8",'2018-11-08 8:23:12');
-INSERT INTO dashboards (id,createdAt,title,updatedAt)  VALUES(9,'2018-11-09 8:23:12',"Title 9",'2018-11-09 8:23:12');
-INSERT INTO dashboards (id,createdAt,title,updatedAt)  VALUES(10,'2018-11-10 8:23:12',"Title 10",'2018-11-10 8:23:12');
-INSERT INTO dashboards (id,createdAt,title,updatedAt)  VALUES(12,'2018-12-11 8:23:12',"Title 11",'2018-12-11 8:23:12');
-INSERT INTO dashboards (id,createdAt,title,updatedAt)  VALUES(13,'2018-12-12 8:23:12',"Title 12",'2018-12-12 8:23:12');
+INSERT INTO dashboards (id,createdAt,title,updatedAt)  VALUES(8,'2018-11-08 8:23:12',"Really great report dashboard",'2018-11-08 8:23:12');
+INSERT INTO dashboards (id,createdAt,title,updatedAt)  VALUES(9,'2018-11-09 8:23:12',"Some awful report",'2018-11-09 8:23:12');
+INSERT INTO dashboards (id,createdAt,title,updatedAt)  VALUES(10,'2018-11-10 8:23:12',"Odd bits and bobs",'2018-11-10 8:23:12');
+INSERT INTO dashboards (id,createdAt,title,updatedAt)  VALUES(12,'2018-12-11 8:23:12',"Regional updates",'2018-12-11 8:23:12');
+INSERT INTO dashboards (id,createdAt,title,updatedAt)  VALUES(13,'2018-12-12 8:23:12',"Sales targets",'2018-12-12 8:23:12');
 
--- src/main/java/com/anaplan/dashboards/DashboardsApplication.java
+++ src/main/java/com/anaplan/dashboards/DashboardsApplication.java
@@ -9,15 +9,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class DashboardsApplication {
 
 	public static void main(String[] args) {
-//		try {
-//			System.out.println("Waiting");
-//			Thread.sleep(5000);
-//			System.out.println("Starting");
-//			
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		try {
+			System.out.println("Waiting");
+			Thread.sleep(5000);
+			System.out.println("Starting");
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		SpringApplication.run(DashboardsApplication.class, args);
 	}
 }
